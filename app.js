44f066c319c7014afcffdbd111e684580e388042
humbaljameel <146485816+HumbalJameel@users.noreply.github.com>
2024-08-08 16:07:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+//CH # 1
+//1.Write a script to greet your website visitor using JS alert box.
+alert("HELLO DEAR VISITOR!I hope you,re doing good.You successfully reach our website official homepage and i beleieve there must be a reason for that so enjoy and look what you want and take your time be comfortable.Have a nice ride. ");
+
+//2. Write a script to display following message on your web page:
+alert("Error!Please enter a valid password.");
+
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land... \n  Happy Coding!");
+
+//4. Write a script to display following messages in sequence:
+alert("Welcome to JS land...");
+alert("Happy Coding!\n Prevent this page from creating additional dialogues.");
+
+//5. Generate the following message through browser’s developer console:
+alert("Hello...I can run JS through my web browser\'s console");
+console.log("Hello...I can run JS through my web browser\'s console");
+
+//CH # 2
+//1. Declare a variable called username.
+var username= "HUMBALJAMEEL";
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var myname="ABU-HUMBAL JAMEEL";
+//3. Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+var message="HELLO WORLD";
+alert("HELLO WORLD");
+//4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var studentName = "ABU-HUMBAL JAMEEL";
+var studentAge = 20;
+var studentGrade = "A+";
+var studentEmail = "humbaljameel@gmail.com";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Grade: " + studentGrade);
+alert("Email: " + studentEmail);
+//5. Write a script to display the following alert using one JS variable:
+var PIZZA,PIZZ,PIZ,PI,P;
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+//6. Declare a variable called email and assign to it a string that
+//represents your Email Address(e.g. example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: use
+//string concatenation)
+var email = "humbaljameel@gmail.com";
+alert("My email address is 'humbaljameel@gmail.com'");
+//7. Declare a variable called book & give it the value “A
+//smarter way to learn JavaScript”.
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book A smarter\n way to learn JavaScript ");
+//8. Write a script to display this in browser through JS.
+document.write("Yah!I can write html content through JavaScript.");
+//9. Store following string in a variable and show in alert and browser through JS.
+var display="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+document.write("<br/>▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+//CH # 3
+//1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+var Age = 20;
+alert("I am 20 years old.")
+//2. Declare & initialize a variable to keep track of how many
+//times a visitor has visited a web page. Show his/her
+//number of visits on your web page. For example: “You
+//have visited this site N times”.
+//3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+var birthYear = "2004"
+document.write("<br/> My birth year is 2004.")
+//4. A visitor visits an online clothing store
+//www.xyzClothing.com . Write a script to store in variables
+//the following information:
+//a. Visitor’s name
+//b. Product title
+//c. Quantity i.e. how many products a visitor wants to
+//order
+//Show the following message in your browser: “John
+//Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var visitorName= "David Johnson";
+var productTitle = "T-shirts";
+var quantity= "4";
+document.write("<br/>David Johnson ordered 4 T-shirts on XYZ Clothing store")
+
+//CH # 4
+// 1. Declare 3 variables in one statement.
+let variable1 = 1, variable2 = "hello", variable3 = true;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+let legal1 = "name"; // Legal
+let legal2 = "age"; // Legal
+let legal3 = "my_variable"; // Legal
+let legal4 = "userName"; // Legal
+let legal5 = "first_name"; // Legal
+
+let illegal1 = 123; // Illegal
+let illegal2 = "123"; // Illegal
+let illegal3 = "  "; // Illegal
+let illegal4 = "first name"; // Illegal
+let illegal5 = "my-variable"; // Illegal
+
+// 3. Display this in your browser
+document.write("<br><h1>Rules for naming JS variables</h1>");
+document.write("<br><br>Variable names can only contain letters, numbers, _ and $. For example: $my_1stVariable");
+document.write("<br>Variable names must begin with a letter, $ or _. For example: $name, _name or name");
+document.write("<br>Variable names are case sensitive");
+document.write("<br>Variable names should not be JS keywords");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
